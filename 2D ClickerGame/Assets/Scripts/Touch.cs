8785d3a1f7f31a52134b94a283bc9d758a329994
CHICHI <smartb12@naver.com>
2024-06-18 11:33:03 +0900

    dmgTxt 3d 오브젝트로 바꿔서 수정중

--- 2D ClickerGame/Assets/Scripts/Touch.cs
+++ 2D ClickerGame/Assets/Scripts/Touch.cs
@@ -1,21 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Touch : MonoBehaviour
 {
     public GameObject rangeOb;
     PolygonCollider2D rangeCol;
 
+    
+    public Animator animator;
+
+    public float damage;
+    public Transform damageTxtPosition;
+    public TextMeshPro dmgTxt;
+
+
     private void Awake()
     {
         rangeCol = GetComponent<PolygonCollider2D>();
+        animator = GetComponent<Animator>();
+        dmgTxt = GetComponent<TextMeshPro>();
     }
 
     private void OnMouseDown()
     {
         Debug.Log("Touch");
-        
+        Attack();
     }
+
+    public void Attack()
+    {
+        Vector3 position = damageTxtPosition.position;
+        CreateDamageText(position, damage);
+    }
+
+    public void CreateDamageText(Vector3 position, float damage)
+    {
+        Instantiate(dmgTxt, position, Quaternion.identity);
+        dmgTxt.text = damage.ToString();
+    }
+
+    
 }
 
